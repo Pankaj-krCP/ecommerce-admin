8aac3b4c20e52b068fba934ef97794127f2f9e52
pankaj <pankajkumar70792@gmail.com>
2024-02-19 19:12:00 +0530

    Updated Main Laout CSS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Resetpassword from "./pages/Resetpassword";
 import Login from "./pages/Login";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
+import Customers from "./pages/Customers";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
         <Route path="forgot-password" element={<Forgotpassword />} />
         <Route path="admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="customers" element={<Customers />} />
         </Route>
       </Routes>
     </Router>
--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,72 +1,192 @@
 import React, { useState } from "react";
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
+  AiOutlineBgColors,
+  AiOutlineDashboard,
+  AiOutlinePicLeft,
+  AiOutlinePicRight,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+} from "react-icons/ai";
+
+import { SiBrandfolder } from "react-icons/si";
+import { FaBloggerB, FaClipboardList } from "react-icons/fa";
+import { BiCategoryAlt } from "react-icons/bi";
+import { ImBlog } from "react-icons/im";
+import { IoIosNotifications } from "react-icons/io";
+
 import { Layout, Menu, Button, theme } from "antd";
+import { Outlet, useNavigate } from "react-router-dom";
 const { Header, Sider, Content } = Layout;
 
 const MainLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
   const {
     token: { colorBgContainer },
   } = theme.useToken();
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
+        <div className="logo">
+          <h2 className="text-white fs-5 text-center py-3 mb-0">
+            <span className="sm-logo">ED</span>
+            <span className="lg-logo">E Digit</span>
+          </h2>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={["1"]}
+          defaultSelectedKeys={["dashboard"]}
+          onClick={({ key }) => {
+            if (key == "signout") {
+            } else {
+              navigate(key);
+            }
+          }}
           items={[
             {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "nav 1",
+              key: "dashboard",
+              icon: <AiOutlineDashboard className="fs-4" />,
+              label: "Dashboard",
             },
             {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "nav 2",
+              key: "customers",
+              icon: <AiOutlineUser className="fs-4" />,
+              label: "Customers",
             },
             {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "nav 3",
+              key: "catalog",
+              icon: <AiOutlineShoppingCart className="fs-4" />,
+              label: "Catalog",
+              children: [
+                {
+                  key: "Product",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "product",
+                },
+                {
+                  key: "product-list",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Product List",
+                },
+                {
+                  key: "brand",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand",
+                },
+                {
+                  key: "brand-list",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand List",
+                },
+                {
+                  key: "category",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category",
+                },
+                {
+                  key: "category-list",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category List",
+                },
+                {
+                  key: "color",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color",
+                },
+                {
+                  key: "color-list",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color List",
+                },
+              ],
+            },
+            {
+              key: "orders",
+              icon: <FaClipboardList className="fs-4" />,
+              label: "Orders",
+            },
+            {
+              key: "blogs",
+              icon: <FaBloggerB className="fs-4" />,
+              label: "Blogs",
+              children: [
+                {
+                  key: "blog",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Add Blog",
+                },
+                {
+                  key: "blog",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Blog-list",
+                },
+                {
+                  key: "blog-category",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Add Blog Category",
+                },
+                {
+                  key: "blog-category-list",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Blog Category List",
+                },
+              ],
+            },
+            {
+              key: "enquiries",
+              icon: <FaClipboardList className="fs-4" />,
+              label: "Enquiries",
             },
           ]}
         />
       </Sider>
       <Layout>
         <Header
+          className="d-flex justify-content-between ps-3 pe-5"
           style={{
             padding: 0,
+            background: colorBgContainer,
           }}
         >
           <Button
-            className="text-white"
+            className="trigger"
             type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            icon={collapsed ? <AiOutlinePicRight /> : <AiOutlinePicLeft />}
             onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: "16px",
-              width: 64,
-              height: 64,
-            }}
           />
+          <div className="d-flex gap-4 align-items-center">
+            <div className="position-relative">
+              <IoIosNotifications className="fs-4" />
+              <span className="badge bg-warning rounded-circle p-1 position-absolute top-10%">
+                3
+              </span>
+            </div>
+            <div className="d-flex gap-3 align-items-center">
+              <div>
+                <img
+                  width={32}
+                  height={32}
+                  src="https://cdn.pixabay.com/photo/2020/07/14/13/07/icon-5404125_1280.png"
+                  alt="profile"
+                />
+              </div>
+              <div>
+                <h5 className="mb-0">Pankaj</h5>
+                <p className="mb-0">pankajkumar70792@gmail.com</p>
+              </div>
+            </div>
+          </div>
         </Header>
         <Content
           style={{
             margin: "24px 16px",
             padding: 24,
             minHeight: 280,
+            background: colorBgContainer,
           }}
         >
-          Content
+          <Outlet />
         </Content>
       </Layout>
     </Layout>
